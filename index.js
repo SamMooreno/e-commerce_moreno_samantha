e8e64f1a37f21c0316f4bba001b9d2c266dfac41
sam <samrey@gmail.com>
2024-09-11 21:40:09 -0600

    claseseis

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hola Mundo 2.0")
+const h51 = document.querySelector(".card-title1"); 
+const text1 = document.querySelector(".card-text1");
+const imagen1= document.querySelector(".card-img-top1");
+h51.innerText = "Plantilla Inglés"
+text1.innerText = "Plantillas para estudiar Inglés con recursos de todo tipo"; 
+imagen1.src="https://tipsparatuviaje.com/wp-content/uploads/2018/08/nueva-york-usa-1152x768.jpg";
+
+const h52 = document.querySelector(".card-title2"); 
+const text2 = document.querySelector(".card-text2");
+const imagen2= document.querySelector(".card-img-top2");
+h52.innerText = "Plantilla Español"
+text2.innerText = "Plantillas para estudiar Español con recursos de todo tipo"; 
+imagen2.src="https://s3.pixers.pics/pixers/700/FO/60/32/13/85/700_FO60321385_35ad286a9320eada5e366a1f206664d6.jpg";
+
+const h53 = document.querySelector(".card-title3"); 
+const text3 = document.querySelector(".card-text3");
+const imagen3= document.querySelector(".card-img-top3");
+h53.innerText = "Plantilla Francés"
+text3.innerText = "Plantillas para estudiar Francés con recursos de todo tipo"; 
+imagen3.src="https://www.1zoom.me/big2/572/322769-zero.jpg";
+
+const h54 = document.querySelector(".card-title4"); 
+const text4 = document.querySelector(".card-text4");
+const imagen4= document.querySelector(".card-img-top4");
+h54.innerText = "Plantilla Coreano"
+text4.innerText = "Plantillas para estudiar Coreano con recursos de todo tipo"; 
+imagen4.src="https://i.pinimg.com/originals/7e/63/67/7e6367e5d9ccb106d5eea3e8407dcd98.jpg";
