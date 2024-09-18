ce59603513ad37c164fb8ab3d807536656f5f049
sam <samrey@gmail.com>
2024-09-17 23:57:27 -0600

    fasediez

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ h1.innerText="Productos";
 const section = document.querySelector("section");
 
 
+
 let arraycards =[];
 
 function card () {
 for(let i=1; i<10; i++){
-let carta= `<div class="card pt-4 d-flex flex-wrap" style="width: 18px">
-<img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top4" alt="Interactiva">
+let carta= `<div class="card pt-4 d-flex flex-wrap" style="width: 18rem;">
+<img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="Interactiva">
 <div class="card-body">
 <h5 class="card-title4">Carro ${i}</h5>
 <p class="card-text4"> Producto ${i} </p>
+<a href="#" class="btn btn-primary">Ver más</a>
 </div></div>`;
 arraycards.push(carta);
 }
